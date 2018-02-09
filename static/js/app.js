f039a5d6eb91b4ebd2179de97433975117c42c4a
Doug Mello <dmello88@gmail.com>
2018-02-08 22:26:54 -0500

    Added close/open search pane functionality to button and moved creation of markers viewmodel function.

--- static/js/app.js
+++ static/js/app.js
@@ -1,42 +1,23 @@
 /* ======= Global Variables ======*/
 var map;
 var largeInfowindow;
+var bounds
 /* ======= Google Map ======= */
 // Function to initilize map.
 function initMap() {
   // Create mapElem
   this.mapElem = document.getElementById('map');
   // Create initial infowdinow variable
-  var largeInfowindow = new google.maps.InfoWindow();
+  largeInfowindow = new google.maps.InfoWindow();
   // Create initial bounds variable
-  var bounds = new google.maps.LatLngBounds();
+  bounds = new google.maps.LatLngBounds();
   markers = [];
   map = new google.maps.Map(this.mapElem, {
     center: {lat: 42.2683199, lng: -71.8174296},
     zoom: 13
   });
   console.log("Map created!");
-  // For loop to create and push markers
-  for (var i = 0; i < locationsData.length; i++) {
-    // Get postion from locaton var
-    var position = locationsData[i].location;
-    // Get title from locaton var
-    var title = locationsData[i].title;
-    // Create a marker per location, and put into markers array.
-    var marker = new google.maps.Marker({
-      map: map,
-      position: position,
-      title: title,
-      animation: google.maps.Animation.DROP,
-      id: i
-    });
-    // Push the markers to the map.
-    markers.push(marker);
-    // Extend the bounds with each marker.
-    bounds.extend(markers[i].position);
-  }
-  // Fit the bounds.
-  map.fitBounds(bounds);
+
   ko.applyBindings(new viewModel());
 };
 
@@ -68,7 +49,42 @@ var locationsData = [
 /* ======= ViewModel ======= */
 var viewModel = function() {
   console.log("The bindings have binded!");
+
+  /*==== Variables ====*/
+
   var self = this;
+  var markers = [];
+
+  /*==== Create Markers ====*/
+
+  // Create markers  // For loop to create and push markers
+  for (var i = 0; i < locationsData.length; i++) {
+    // Get postion from locaton var
+    var position = locationsData[i].location;
+    // Get title from locaton var
+    var title = locationsData[i].title;
+    // Create a marker per location, and put into markers array.
+    var marker = new google.maps.Marker({
+      map: map,
+      position: position,
+      title: title,
+      animation: google.maps.Animation.DROP,
+      id: i
+    });
+    // Push the markers to the map.
+    markers.push(marker);
+    // Extend the bounds with each marker.
+    bounds.extend(markers[i].position);
+    // Create an onclick event to open an infowindow at each marker.
+    marker.addListener('click', function() {
+      populateInfoWindow(this, largeInfowindow);
+    });
+  }
+  // Fit the bounds.
+  map.fitBounds(bounds);
+
+  /*==== ko observables ====*/
+
   // Create ko observable for search input.
   self.searchLocation = ko.observable();
   // Create KO observable array for markers
@@ -77,20 +93,51 @@ var viewModel = function() {
   markers.forEach(function(marker) {
     self.markerList.push(marker);
   });
+  // Create ko obersable boolean for search panel visibility.
+  self.showSearchPanel = ko.observable(true);
+  var searchButtonStatus = true;
   // Create KO observable array for locatoins
   self.locationList = ko.observableArray([]);
   // Go through locations array and create a observable array.
   locationsData.forEach(function(location){
     self.locationList.push({name: location.title});
   });
-  // Hide pane function
-  self.hidePane = function() {
+
+  /*==== KO Functions ====*/
+
+  // Toggle pane function
+  self.toggleSearchPane = function() {
+    if (searchButtonStatus) {
+      self.showSearchPanel(false);
+      searchButtonStatus = false;
+      $("#hidePaneButton").attr("title","Show Search Pane");
+    } else {
+      self.showSearchPanel(true);
+      searchButtonStatus = true;
+      $("#hidePaneButton").attr("title","Hide Search Pane");
+    }
   };
   // Filter search function
   self.filterSearch = function() {
+    console.log("Clicked!");
   };
   // Select marker function
   self.selectMarker = function() {
     console.log("Clicked!");
   };
+
+  /*==== functions ====*/
+
+  function populateInfoWindow(marker, infowindow) {
+    // Check to make sure the infowindow is not already opened on this marker.
+    if (infowindow.marker != marker) {
+      infowindow.marker = marker;
+      infowindow.setContent('<div>' + marker.title + '</div>');
+      infowindow.open(map, marker);
+      // Make sure the marker property is cleared if the infowindow is closed.
+      infowindow.addListener('closeclick',function(){
+        infowindow.setMarker = null;
+      });
+    }
+  }
 };
